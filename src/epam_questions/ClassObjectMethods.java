55bb0043fce156c76376dcd12b0145823a5b5c58
Valery Daroshka <Valery_Daroshka@epam.com>
2021-09-01 21:05:42 +0300

    first commit

---
+++ src/epam_questions/ClassObjectMethods.java
@@ -0,0 +1,11 @@
+package epam_questions;
+
+public class ClassObjectMethods {
+    public static void main(String[] args) {
+        Object object = new Object();
+
+
+
+    }
+}
+
---
+++ src/epam_questions/QuestionsList.java
@@ -0,0 +1,4 @@
+package epam_questions;
+
+public class QuestionsList {
+}
